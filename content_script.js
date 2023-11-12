3bec75ed8c7caac9fe545482c00cbfe1ff8a53ec
dd <dd@dd>
2023-11-12 15:00:07 +0900

    feat: update new button class
    Signed-off-by: dd <dd@dd>

--- content_script.js
+++ content_script.js
@@ -1,6 +1,7 @@
 function SkipAd() {
     var array = Array.from(document.getElementsByClassName('ytp-ad-skip-button'))
         .concat(Array.from(document.getElementsByClassName('ytp-ad-overlay-close-button')))
+        .concat(Array.from(document.getElementsByClassName('ytp-ad-skip-button-modern')))
         .forEach(function(element) {
             element.click();
         });
